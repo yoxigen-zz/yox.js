7ce3f256a8fb7e74e441150a6c76e2a49150ed40
Yossi Kolesnicov <yoxigen@gmail.com>
2012-03-09 17:08:01 +0200

    Events manager, can be used to add and trigger events, with or without namespaces. Is used by modules to communicate. This is a generic object, can be used in any project.

---
+++ src/yox.eventsHandler.js
@@ -0,0 +1,51 @@
+yox.eventsHandler = function(){
+    var namespaces = {
+        _default: {}
+    };
+
+    this.triggerEvent = function(eventName, data, id){
+        var eventNameParts = eventName.split("."),
+            eventType = eventNameParts[0],
+            namespaceName = eventNameParts[1];
+
+        if (namespaceName){
+            var namespace = namespaces[namespaceName];
+            if (namespace){
+                var namespaceEvents = namespace[eventType];
+                if (namespaceEvents){
+                    for(var i=0, eventHandler; eventHandler = namespaceEvents[i]; i++){
+                        eventHandler.call(this, data, id);
+                    }
+                }
+            }
+        }
+
+        var noNamespacedEvents = namespaces._default[eventType];
+        if (noNamespacedEvents){
+            for(var i=0, eventHandler; eventHandler = noNamespacedEvents[i]; i++){
+                eventHandler.call(this, data, id);
+            }
+        }
+    };
+
+    this.addEventListener = function(eventName, eventHandler){
+        var eventNameParts = eventName.split("."),
+            eventType = eventNameParts[0],
+            namespaceName = eventNameParts[1],
+            namespace;
+
+        if (namespaceName){
+            namespace = namespaces[namespaceName];
+            if (!namespace)
+                namespace = namespaces[namespaceName] = { };
+        }
+        else
+            namespace = namespaces._default;
+
+        var event = namespace[eventType];
+        if (!event)
+            event = namespace[eventType] = [];
+
+        event.push(eventHandler);
+    }
+};
