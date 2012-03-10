89598e757ec3fd3b6199a2c5dae881ddf760507a
Yossi Kolesnicov <yoxigen@gmail.com>
2012-03-10 17:39:11 +0200

    New drag and drop features.

--- build/yox.js
+++ build/yox.js
@@ -460,6 +460,53 @@ yox.data.prototype.addDataSource(function(){
 	};
 }());
 yox.data.prototype.addDataSource((function(){
+    var dataSourceName = "files",
+        createObjectURL;
+
+    if (window.URL && window.URL.createObjectURL)
+        createObjectURL = function(file){ return window.URL.createObjectURL(file); };
+    else if (window.webkitURL && window.webkitURL.createObjectURL){
+        createObjectURL = function(file){
+            return window.webkitURL.createObjectURL(file);
+        }
+    }
+    else
+        createObjectURL = function(file){ return null; }
+
+    return {
+        name: dataSourceName,
+        match: function(source){
+            return false;
+        },
+        load: function(source, callback){
+            var items = [];
+            for(var i=0, file; file = source.files[i]; i++){
+                if (/^image\//.test(file.type)){
+                    var url = createObjectURL(file);
+                    items.push({
+                        url: url,
+                        type: "image",
+                        title: file.name || file.fileName,
+                        thumbnail: {
+                            src: url
+                        }
+                    });
+                }
+            }
+
+            var data = {
+                items: items,
+                source: source,
+                sourceType: dataSourceName,
+                createThumbnails: true
+            };
+
+            if (callback)
+                callback(data);
+        }
+    };
+})());
+yox.data.prototype.addDataSource((function(){
 	var dataSourceName = "picasa",
         picasaRegex = /^https?:\/\/picasaweb\.google\./,
         picasaMatchRegex = /https?:\/\/picasaweb\.google\.\w+\/([^\/#\?]+)\/?([^\/#\?]+)?(\?([^#]*))?/,
@@ -1189,8 +1236,10 @@ yox.data.prototype.addDataSource((function(){
 
 
             dataSource.addEventListener("loadSources", function(sources){
-                self.clear();
-                this.itemCount = 0;
+                if (!self.options.allowAppend){
+                    self.clear();
+                    this.itemCount = 0;
+                }
                 renderSources(sources);
             });
 
@@ -1266,6 +1315,7 @@ yox.data.prototype.addDataSource((function(){
             this.triggerEvent("create", { thumbnails: thumbnailElements, items: source.items });
         },
         defaults: {
+            allowAppend: true, // If true, new data sources cause thumbnails to be added rather than replace the existing thumbnails.
             renderThumbnailsTitle: true,
             selectedThumbnailClass: "selectedThumbnail",
             thumbnailClass: "yoxthumbnail"
