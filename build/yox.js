e992313a0af2ab78d15ab2067ee4d07476354909
Yossi Kolesnicov <yoxigen@gmail.com>
2012-03-15 00:24:24 +0200

    Bug fixes, mostly.

--- build/yox.js
+++ build/yox.js
@@ -12,7 +12,10 @@ Yox.prototype = {
                 data;
 
             if (this.options.data){
-                data = new yox.data(this.options.data);
+                if (this.options.data instanceof yox.data)
+                    data = this.options.data;
+                else
+                    data = new yox.data(this.options.data);
             }
 
             if (!themeConstructor)
@@ -347,7 +350,7 @@ yox.data.prototype = {
     clear: function(){
         this.triggerEvent("removeSources", this.data);
         this.triggerEvent("clear");
-        data = [];
+        this.data = [];
     },
     findDataSource: function(sourceData){
         for(var dataSourceName in yox.data.sources){
@@ -1381,7 +1384,7 @@ yox.data.sources.picasa = (function(){
 
     window.yox.thumbnails = yox.thumbnails;
 })(jQuery);
-(function($){
+﻿(function($){
     yox.utils.css.addJqueryCssHooks(["transition", "transitionDuration", "transform", "transformStyle", "backfaceVisibility", "perspective"]);
 
 	yox.view = function(container, options, cache){
@@ -1687,8 +1690,8 @@ yox.data.sources.picasa = (function(){
                         self.addEventListener(eventName, eventHandlers);
                 }
 
-                this.options.data && this.addDataSources(this.options.data);
                 createViewer(this);
+                this.options.data && this.addDataSources(this.options.data);
 
                 if (this.options.enableKeyboard)
                     this.enableKeyboard();
@@ -2521,8 +2524,9 @@ yox.theme.prototype = {
             return new moduleConstructor(container, moduleOptions);
         }
 
-        for(var moduleName in this.config){
-            var moduleOptions = this.config[moduleName],
+        var modulesConfig = $.extend(true, {}, this.config, options.modules);
+        for(var moduleName in modulesConfig){
+            var moduleOptions = modulesConfig[moduleName],
                 moduleConstructor = yox[moduleName];
 
             if (!moduleConstructor)
@@ -2544,6 +2548,195 @@ yox.theme.prototype = {
     config: {},
     modules: {}
 }
+yox.themes.inline2 = function(data, options){
+    var self = this,
+        elements,
+        mousemoveTimeoutId,
+        isFullScreen,
+        isFullScreenApi,
+        galleryOriginalHeight,
+        thumbnailsRect,
+        lastPos;
+
+    this.name = "inline2";
+    this.config = {
+        view: {
+            enableKeyboard: true,
+            enlarge: true,
+            resizeMode: "fill",
+            transition: "fade",
+            transitionTime: 300,
+            events: {
+                cacheStart: function(e, item){ elements.loader.style.display = "inline" },
+                cacheEnd: function(e, item){ elements.loader.style.display = "none" },
+                "click.thumbnails": function(e){ this.selectItem(e.index); },
+                "init.view": function(){
+                    this.selectItem(0);
+                }
+            }
+        },
+        thumbnails: {
+            events: {
+                beforeSelect: function(e){
+                    this.select(e.newItem.id - 1);
+                },
+                loadItem: function(item){
+                    $(this.thumbnails[item.id - 1]).addClass("loaded");
+                },
+                "create.thumbnails": function(){ this.select(0); }
+            }
+        },
+        scroll: {
+            events: {
+                "create.thumbnails": function(e, id){
+                    this.update();
+                },
+                "select.thumbnails": function(e){
+                    this.scrollTo(e, { centerElement: true, time: .5 });
+                },
+                resize: function(){ this.update() }
+            },
+            elements: $("." + this.getThemeClass("thumbnailsPanel"), elements.thumbnailsPanel),
+            pressedButtonClass: "enabledThumbnailsButton"
+        }
+    };
+
+    function emptyFunction(){};
+    document.cancelFullScreen = document.cancelFullScreen || document.mozCancelFullScreen || document.webkitCancelFullScreen || emptyFunction;
+    HTMLElement.prototype.requestFullScreen = HTMLElement.prototype.requestFullScreen || HTMLElement.prototype.mozRequestFullScreen || HTMLElement.prototype.webkitRequestFullScreen || emptyFunction;
+    isFullScreenApi = document.cancelFullScreen !== emptyFunction;
+
+    function onFullScreenChange(e){
+        if (isFullScreenApi)
+            isFullScreen = !isFullScreen;
+
+        if (isFullScreen){
+            mousemoveTimeoutId = setTimeout(function(){
+                elements.$thumbnails.css({ opacity: 0 });
+                document.body.style.cursor = "none"; }
+            , 3000);
+
+            self.modules.view.option("resizeMode", "fit");
+        }
+        else{
+            clearTimeout(mousemoveTimeoutId);
+            elements.$thumbnails.css("opacity", "1");
+            elements.gallery.style.height = galleryOriginalHeight + "px";
+            document.body.style.cursor = "default";
+            self.modules.view.option("resizeMode", "fill");
+        }
+
+        onResize();
+
+        var $window = $(window),
+            windowEventCaller = isFullScreen ? $window.on : $window.off;
+
+        windowEventCaller.call($window, "mousemove", onMouseMove);
+        if (!isFullScreenApi)
+            windowEventCaller.call($(document), "keydown", onKeyDown);
+    }
+
+    if (isFullScreenApi)
+        document.addEventListener(document.mozCancelFullScreen ? "mozfullscreenchange" : "webkitfullscreenchange", onFullScreenChange, false);
+
+    function toggleFullScreen(){
+        if (isFullScreenApi){
+            if (isFullScreen)
+                document.cancelFullScreen();
+            else{
+                elements.gallery.style.height = "100%";
+                elements.gallery.requestFullScreen();
+            }
+        }
+        else{
+            isFullScreen = !isFullScreen;
+            elements.gallery.style.position = isFullScreen ? "fixed" : "relative";
+            elements.gallery.style.height = isFullScreen ? "100%" : galleryOriginalHeight;
+            elements.gallery.style.border = isFullScreen ? "none" : "solid 1px Black";
+            elements.gallery.style.zIndex = isFullScreen ? "100" : "1";
+            onFullScreenChange();
+        }
+    }
+
+    function onResize(){
+        self.modules.view.update(true);
+        thumbnailsRect = elements.thumbnails.getClientRects()[0];
+    }
+
+    function onKeyDown(e){
+        if (e.keyCode === 27)
+            toggleFullScreen();
+    }
+
+    function onMouseMove(e){
+        if (!lastPos || e.pageX < lastPos.x - 4 || e.pageX > lastPos.x + 4 || e.pageY < lastPos.y - 4 || e.pageY > lastPos.y + 4){
+            clearTimeout(mousemoveTimeoutId);
+            elements.$thumbnails.css("opacity", "1");
+            document.body.style.cursor = "default";
+
+            mousemoveTimeoutId = setTimeout(function(){
+                if (e.pageY >= thumbnailsRect.top)
+                    return;
+
+                elements.$thumbnails.css({ opacity: 0 });
+                document.body.style.cursor = "none";
+            }, 2000);
+        }
+        lastPos = { x: e.pageX, y: e.pageY };
+    }
+
+    this.create = function(container){
+        $(container).addClass(this.getThemeClass());
+
+        elements = {
+            container: container,
+            gallery: document.createElement("div"),
+            viewer: document.createElement("div"),
+            thumbnails: document.createElement("div"),
+            thumbnailsPanel: document.createElement("div"),
+            loader: document.createElement("loader"),
+            description: document.createElement("p")
+
+        };
+
+        elements.$thumbnails = $(elements.thumbnails);
+        var thumbnailsBtnClass = this.getThemeClass("thumbnailsBtn");
+        elements.thumbnailsPanel.className = this.getThemeClass("thumbnailsPanel");
+        elements.thumbnailsPanel.innerHTML =
+            '<a href="#" class="' + thumbnailsBtnClass + ' ' + thumbnailsBtnClass + '_left" data-yoxscroll-holdstart="scroll-left" data-yoxscroll-click="page-left"></a>' +
+            '<a href="#" class="' + thumbnailsBtnClass + ' ' + thumbnailsBtnClass + '_right" data-yoxscroll-holdstart="scroll-right" data-yoxscroll-click="page-right"></a>';
+        elements.thumbnailsPanel.appendChild(elements.thumbnails);
+
+        container.appendChild(elements.gallery);
+        elements.gallery.appendChild(elements.viewer);
+        elements.gallery.appendChild(elements.description);
+        container.appendChild(elements.thumbnailsPanel);
+        elements.gallery.appendChild(elements.loader);
+
+        elements.viewer.className = this.getThemeClass("viewer") + " yoxview";
+        elements.gallery.className = this.getThemeClass("gallery");
+        elements.thumbnails.className = this.getThemeClass("thumbnails") + " yoxthumbnails yoxscroll";
+        elements.loader.className = this.getThemeClass("loader") + " yoxloader";
+        elements.description.className = this.getThemeClass("description");
+
+        galleryOriginalHeight = elements.gallery.clientHeight;
+        thumbnailsRect = elements.thumbnails.getClientRects()[0];
+
+        $(elements.gallery).on("dblclick", toggleFullScreen);
+        $(window).on("resize", onResize);
+    };
+
+    this.destroy = function(){
+        $(elements.container).removeClass(this.getThemeClass());
+        elements.container.removeChild(elements.gallery);
+        elements.container.removeChild(elements.thumbnailsPanel);
+        elements = null;
+
+        $(window).off("resize", onResize);
+    };
+}
+
+yox.themes.inline2.prototype = new yox.theme();
 yox.themes.inline = function(data, options){
     var self = this,
         elements,
