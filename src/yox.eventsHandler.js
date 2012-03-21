99140321b70c157f51e277f007adb6f8ddd325d9
Yossi Kolesnicov <yoxigen@gmail.com>
2012-03-22 00:44:47 +0200

    Added removeEventListener

--- src/yox.eventsHandler.js
+++ src/yox.eventsHandler.js
@@ -47,5 +47,33 @@ yox.eventsHandler = function(){
             event = namespace[eventType] = [];
 
         event.push(eventHandler);
+    };
+
+    this.removeEventListener = function(eventName, eventHandler){
+        var eventNameParts = eventName.split("."),
+            eventType = eventNameParts[0],
+            namespaceName = eventNameParts[1],
+            namespace,
+            foundHandler = false;
+
+        if (namespaceName){
+            namespace = namespaces[namespaceName];
+            if (!namespace)
+                namespace = namespaces[namespaceName] = { };
+        }
+        else
+            namespace = namespaces._default;
+
+        var event = namespace[eventType];
+        if (event && event.length){
+            for(var i=event.length; i--;){
+                if (event[i] === eventHandler){
+                    event.splice(i, 1);
+                    foundHandler = true;
+                }
+            }
+        }
+
+        return foundHandler;
     }
 };
