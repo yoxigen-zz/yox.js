ad6a50b4d06f261654a8c86e411e0f80fbadb009
yossi.k <yossi.kolsnikov@conduit.com>
2012-04-18 16:27:35 +0300

    Added JSDoc documentation.

--- src/modules/data/yox.data.js
+++ src/modules/data/yox.data.js
@@ -1,3 +1,11 @@
+/**
+ * Data module, responsible for retrieving and holding data used by other modules.
+ * Data is retrieved through data sources, which are sub-modules of the data module.
+ * Each data source retrieves data from an external resource, such as external APIs, the DOM or HTML5 APIs (such as the File API).
+ * Data can be cache in localStorage to improve loading times on recurring uses from external APIs.
+ * yox.data can be used independently as an easy interface to external API's.
+ * @param options
+ */
 yox.data = function(options){
     this.data = [];
     this.options = $.extend(true, {}, this.defaults, options);
@@ -17,6 +25,13 @@ yox.data.prototype = {
     defaults: {
         cache: false // Set this to true to enable caching on localStorage. Cache is used only for external sources - it saves the data retrieved from the source (what's return from the source's load() method).
     },
+    /**
+     * Add sources to the data object.
+     * When the added sources are finished loading (when all the data sources' load() methods calls their callbacks),
+     * the 'loadSources' event is fired.
+     *
+     * @param {Object[]|Object} sourceArr An array of source data objects (or a single object) to add.
+     */
     addSources: function(sourceArr){
         var deferredPromises = [],
             sources = sourceArr instanceof Array ? sourceArr : arguments,
@@ -39,11 +54,19 @@ yox.data.prototype = {
             self.triggerEvent("loadSources", Array.prototype.slice.call(arguments, 0));
         });
     },
+    /**
+     * Clears the data object of all data.
+     * Triggers two events: 'removeSources', with the removed data, and 'clear', with no data.
+     */
     clear: function(){
         this.triggerEvent("removeSources", this.data);
         this.triggerEvent("clear");
         this.data = [];
     },
+    /**
+     * Returns a count of the total items in the data object (from all sources).
+     * @return {Number}
+     */
     countItems: function(){
         var totalItemsCount = 0;
         for(var i=this.data.length; i--;){
@@ -52,6 +75,13 @@ yox.data.prototype = {
 
         return totalItemsCount;
     },
+    /**
+     * Given an object with source data (the same as given to the 'addSources' method or to the yox.data constructor),
+     * returns the data source sub-module which matches the source data, using the data source's 'match' method.
+     *
+     * @param sourceData
+     * @return {Object} The data source object that matches the source data, or null if none found.
+     */
     findDataSource: function(sourceData){
         for(var dataSourceName in yox.data.sources){
             var dataSource = yox.data.sources[dataSourceName];
@@ -59,17 +89,35 @@ yox.data.prototype = {
             if (dataSource.match(sourceData))
                 return dataSource;
         }
+
+        return null;
     },
+    /**
+     * Returns an array of sources holding the module's data.
+     * @return {Array}
+     */
     getData: function(){ return this.data; },
+    /**
+     * Returns a data source, by name, simply gets yox.data.sources[dataSourceName].
+     * @param {String} dataSourceName The name of the data source, such as 'picasa', 'flickr' or 'html'.
+     * @return {*}
+     */
     getDataSource: function(dataSourceName){
         return yox.data.sources[dataSourceName];
     },
+    /**
+     * Given a valid source data (a matching data source sub-module is found), returns a jQuery Deferred object
+     * which resolves once the load() method of the data source sub-module called its callback.
+     *
+     * @param {Object} source
+     * @return {jQuery.Deferred} A jQuery.Deferred object, or null if no data source sub-module exists for the given data.
+     */
     loadSource: function(source){
         var dataSource = source.type ? this.getDataSource(source.type) : this.findDataSource(source),
             self = this;
 
         if (!dataSource)
-            return;
+            return null;
 
         var dfd = $.Deferred(),
             onLoadSource = function(sourceData){
@@ -89,12 +137,26 @@ yox.data.prototype = {
         }
         return dfd;
     },
+    /**
+     * Replaces all data in the yox.data object with the given source(s).
+     * @param {Object} sources One or more source data objects (for more than one, pass the method multiple params).
+     */
     source: function(sources){
         this.clear();
         this.addSources.apply(this, arguments);
     },
+    /**
+     * Saves or retrieves from localStorage a source's data object.
+     * options.cache must be set to true and to use this method, otherwise it just returns false.
+     * If there is no window.localStorage object, the method also returns false.
+     * If no data is specified, the method returns the available data for the given source.
+     *
+     * @param {Object} source The source data object to store (defines the localStorage key name)
+     * @param {object} data The source's data to save to localStorage.
+     * @return {Boolean|Object} true: data was saved, false: the method failed, JSON: the retrieved data.
+     */
     store: function(source, data){
-        if (!this.options.cache || !window.localStorage || !source.url)
+        if (!this.options.cache || !window.localStorage || !source || !source.url)
             return false;
 
         var keyName = this.namespace + ".source." + source.url;
@@ -107,5 +169,6 @@ yox.data.prototype = {
             return null;
         }
         window.localStorage.setItem(keyName, JSON.stringify(data));
+        return true;
     }
 };
